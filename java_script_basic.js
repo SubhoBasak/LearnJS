0f575915526db8e3f2b248e7f7a262cd5e76cc4d
SubhoBasak <subhobasak50@gmail.com>
2021-05-01 21:35:07 +0530

    Initial Commit

---
+++ java_script_basic.js
@@ -0,0 +1,81 @@
+// print to console
+console.log("Hello World")
+console.log(123)
+console.log(99.99)
+
+// comment
+
+// this is a single line comment
+
+/*
+this is a
+multi line
+comment
+*/
+
+// variable declare
+var x = 123
+var y = 'JavaScript'
+var z = 99.99
+
+let a = 123
+let b = 'JavaScript'
+let c = 99.99
+
+const m = 123
+const n = 'JavaScript'
+const o = 99.99
+
+
+// operators
+var p = 2
+var q = 3
+var r = null;
+
+r = p + q;
+r = p - q;
+r = p * q;
+r = p / q;
+r = p % q;
+r = p ** q;
+
+p++;
+++p;
+
+p += 5;
+p -= 5;
+p *= 5;
+p /= 5;
+p %= 5;
+p **= 5;
+
+// some extra features of javascript
+console.log('1' + 1) // output will be 2
+console.log('1' + '1') // output will be '11'
+
+// comparison
+var p = '1'
+var q = 1
+
+// compare only value not type
+p == '1' // true
+p == 1 // true
+q == '1' // true
+q == 1 // true
+p == q // true
+
+// compare value and type both
+p === '1' // true
+p === 1 // false
+q === 1 // true
+q === '1' // false
+p === q // false
+
+p != '0' // true
+p != 0 // true
+p != q // false
+
+p !== 1 // true
+p !== q // true
+
+
